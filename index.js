e7a133ad08ea708e0f93c6195f77030f51654cef
DamiTunjiAjayi <damilaretunjiajayi@gmail.com>
2025-02-26 12:26:16 +0100

    Program to print 'Hello 'World' and to log the current date

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// My first JavaScript code
+// Function to get the sum of two values
 function addition(firstValue, secondValue) {
     return firstValue + secondValue
     }
     
     addition(11,34);
     console.log(addition(11,34));
+
+//print 'Hello World'
+console.log('Hello World');
+
+//create variable to get currentDate and log the Date Time
+const currentDate = new Date ();
+
+console.log(currentDate);
