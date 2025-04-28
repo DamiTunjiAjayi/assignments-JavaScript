ab60e12fe5bf0cafe6346837a4d5df10277a3fac
DamiTunjiAjayi <damilaretunjiajayi@gmail.com>
2025-04-28 22:34:01 +0100

    Week9 Assignment

---
+++ question1-week9rectangle.js
@@ -0,0 +1,13 @@
+/**
+ * Calculates the area of a rectangle.
+ * @param {number} width  – the rectangle’s width
+ * @param {number} height – the rectangle’s height
+ * @returns {number} area (width × height)
+ */
+function calculateArea(width, height) {
+    return width * height;
+  }
+  
+  // Export the function so it can be imported elsewhere
+  module.exports = calculateArea;
+  
---
+++ question2-week9Area.js
@@ -0,0 +1,9 @@
+// Import the calculateArea function from question1-week9Rectangle.js
+const calculateArea = require('./question1-week9rectangle');
+
+// Example usage:
+const width = 7;
+const height = 3;
+const area = calculateArea(width, height);
+
+console.log(`A rectangle ${width}×${height} has area: ${area}`);
