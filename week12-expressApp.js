439c19cd75828acf85991bee81b480f1e4450069
DamiTunjiAjayi <damilaretunjiajayi@gmail.com>
2025-06-17 12:10:29 +0100

    Week13-Assignment

--- week12-expressApp.js
+++ week12-expressApp.js
@@ -8,6 +8,22 @@ app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
 
+// Route with URL parameter for user ID
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID is ${userId}`);
+});
+
+// Route with query parameter for search
+app.get('/search', (req, res) => {
+  const query = req.query.q;
+  if (query) {
+    res.send(`Search query is "${query}"`);
+  } else {
+    res.send('Please provide a search query using ?q=your+term');
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Express server listening at http://localhost:${PORT}/`);
